08bb6a1c96b9ba826b3032a199cf73f199da73b6
Tobias Tengler <ttengler@artiso.com>
2020-10-28 10:26:05 +0100

    Add express

--- imagefeed/service/src/index.ts
+++ imagefeed/service/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
