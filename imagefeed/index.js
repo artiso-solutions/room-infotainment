b3fd934bd57c0e3dc3d845bb234de99b4903ac5b
Alesia Bragagnolo <abragagnolo@artiso.com>
2020-10-28 14:58:34 +0100

    [WIP] add paper.Js initialize

--- imagefeed/index.js
+++ imagefeed/index.js
@@ -1,4 +1,4 @@
-let weather = 'Rain';
+let weather = 'Clouds';
 let colors = ['#A14893', '#6984BE', '#ACE563', '#FFF600', '#FF4300', '#FF0000'];
 let minTemp = -30;
 let maxTemp = 50;
@@ -24,22 +24,38 @@ function calcTempColor(temperature) {
   return col1.add(colVec);
 }
 
-/* let circ = new Path.Circle([300,300], [150,150]);
-circ.fillColor = calcTempColor(-5);
-
-let circ2 = new Path.Circle([500,300], [150,150]);
-circ2.fillColor = calcTempColor(-7); */
-
 var TEMP = 0;
 // The amount of circles we want to make:
 var colorNrs = 4;
-var count = 600;
+var count = 60;
 var subcount = count / colorNrs;
+
+// Get a reference to the canvas object
+var canvas = document.getElementById('myCanvas');
+// Create an empty project and a view for the canvas:
+paper.setup(canvas);
 
 // Create a symbol, which we will use to place instances of later:
 let path;
 switch (weather) {
   case 'Rain':
+  case 'Drizzle':
+  case 'Thunderstorm':
+    path = new Path(
+      'M85.91,66.47A43,43,0,0,1,0,66.47C0,42.75,43,0,43,0S85.91,42.75,85.91,66.47Z'
+    );
+    break;
+  case 'Clouds':
+    path = new Path(
+      'M29.07,56.21C29.39,33,52.81,16.6,74.72,24.4A40.53,40.53,0,0,1,99,3.89a39,39,0,0,1,23.84-.05,41,41,0,0,1,29,39c4.23,2.05,8.52,3.6,12.25,6a33.88,33.88,0,0,1-14.52,61.61,55.85,55.85,0,0,1-7.2.39q-55.57,0-111.16,0c-11.94,0-21.29-4.9-26.59-16-8.13-17,3.82-37.22,22.62-38.48C27.84,56.36,28.46,56.27,29.07,56.21Z'
+    );
+    break;
+  case 'Clear':
+    path = new Path(
+      'M85.91,66.47A43,43,0,0,1,0,66.47C0,42.75,43,0,43,0S85.91,42.75,85.91,66.47Z'
+    );
+    break;
+  case 'Snow':
     path = new Path(
       'M85.91,66.47A43,43,0,0,1,0,66.47C0,42.75,43,0,43,0S85.91,42.75,85.91,66.47Z'
     );
@@ -78,6 +94,8 @@ for (var i = 0; i < count; i++) {
   placedSymbol.scale(i / count);
 }
 
+paper.view.draw();
+
 // The onFrame function is called up to 60 times a second:
 function onFrame(event) {
   // Run through the active layer's children list and change
@@ -87,7 +105,7 @@ function onFrame(event) {
 
     // Move the item 1/20th of its width to the right. This way
     // larger circles move faster than smaller circles:
-    item.position.y += item.bounds.width / 2;
+    item.position.y += item.bounds.width / 20;
 
     // If the item has left the view on the right, move it back
     // to the left:
